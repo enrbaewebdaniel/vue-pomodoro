e904b1153c85bc86f0bbfdaf8a84bd4ef7918977
Dani <enrbaewebdaniel@gmail.com>
2022-04-04 22:32:46 +0200

    add new funcionalitics and try fix rerender minutes and seconds

--- funtions.js
+++ funtions.js
@@ -1,22 +1,89 @@
 const app = new Vue({
-    el: '#app',
-    data: {
-        actualFase: 4,
-        minutes: "00",
-        seconds: "00",
-        active: false,
-        ciclo: 0
-    },
-    computed: {
-        currentFase(){
-            const fase = {
-                0: "Pausado",
-                1: "En Curso",
-                2: "Finalizado",
-                4: "Esperando..."
-            }
+  el: "#app",
+  data: {
+    minutosPerCiclo: 25,
+    minutosPerDescanso: 25,
+    actualFase: 4,
+    minutes: 0,
+    seconds: 0,
+    paused: false,
+    ciclo: 0,
+    cicloCounter: 0,
+  },
 
-            return fase[this.actualFase];
+  methods: {
+    resetTimer() {
+      this.minutes = 0;
+      this.seconds = 0;
+      this.cicloCounter = 0;
+      this.actualFase = 4;
+      this.paused = false;
+    },
+    waiter(minutes) {
+      this.minutes = minutes;
+      this.seconds = 0;
+
+      console.log("entra");
+
+      while (this.minutes <= this.minutosPerCiclo) {
+        this.seconds = 60;
+        console.log("entra1");
+        while (this.seconds <= 60) {
+          console.log("entra2");
+          this.syncDelay(1000);
+          this.seconds--;
         }
-    }
+
+        this.minutes--;
+      }
+    },
+    syncDelay(milliseconds) {
+      var start = new Date().getTime();
+      var end = 0;
+      while (end - start < milliseconds) {
+        end = new Date().getTime();
+      }
+    },
+    startTimer() {
+      this.resetTimer();
+      this.cicloCounter++;
+      this.actualFase = 1;
+
+      this.minutes = this.minutosPerCiclo;
+
+      while (this.cicloCounter <= this.ciclo) {
+        console.log("va a empezar");
+
+        this.waiter(this.minutosPerCiclo);
+
+        alert("Empieza el tiempo de descanso");
+
+        this.waiter(this.minutosPerDescanso);
+
+        this.cicloCounter++;
+      }
+
+      this.ciclo = 0;
+      this.actualFase = 4;
+    },
+  },
+  computed: {
+    currentFase() {
+      const fase = {
+        0: "Pausado",
+        1: "En Curso",
+        3: "Descanso",
+        4: "Esperando...",
+      };
+
+      return fase[this.actualFase];
+    },
+    fillMinutes() {
+      return this.minutes.toString().padStart(2, 0);
+    },
+
+    fillSeconds() {
+      return this.seconds.toString().padStart(2, 0);
+    },
+  },
 });
